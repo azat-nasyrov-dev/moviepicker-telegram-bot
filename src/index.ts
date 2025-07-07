044ff31614a92d337677d29590383b70ba571f54
Azat Nasyrov <azat.nasyrov31@gmail.com>
2025-07-07 19:29:41 +0600

    get a random movie; #3

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
-import { Bot } from "grammy";
+import { Bot, InputMediaBuilder } from "grammy";
 import "dotenv/config";
+import { getRandom } from "./services/moviesearch";
 
 const bot = new Bot(process.env.TELEGRAM_TOKEN || "");
 
 bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
-bot.on("message", (ctx) => ctx.reply("Got another message!"));
+bot.on("message", async (ctx) => {
+    const entity = await getRandom();
+    const media = InputMediaBuilder.photo(entity.poster.url, {
+        caption: `${entity.name} \n${entity.shortDescription}`,
+    });
+
+    ctx.replyWithMediaGroup([media]);
+});
 
 bot.start();
